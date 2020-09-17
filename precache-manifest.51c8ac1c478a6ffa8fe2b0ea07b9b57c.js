0cbb8cd2fa5c6bdb6da9113c745cc996ba9c121e
Shweta Kale <shwetakale144@gmail.com>
2020-09-17 16:41:56 +0530

    Updates

---
+++ precache-manifest.51c8ac1c478a6ffa8fe2b0ea07b9b57c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7527a60192d81e6ed23b858b45acae8",
+    "url": "/quizApp/index.html"
+  },
+  {
+    "revision": "d6b7fc08b0c882f10016",
+    "url": "/quizApp/static/css/main.5b6273fa.chunk.css"
+  },
+  {
+    "revision": "179d2f3dae1f16349ab9",
+    "url": "/quizApp/static/js/2.5b118441.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quizApp/static/js/2.5b118441.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6b7fc08b0c882f10016",
+    "url": "/quizApp/static/js/main.fe699460.chunk.js"
+  },
+  {
+    "revision": "9512da0e8f2950f2af26",
+    "url": "/quizApp/static/js/runtime-main.eaf234f1.js"
+  }
+]);
